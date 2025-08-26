5e6b8e0ffdd8873fadfc676b8078a3aed3fbad0a
Leo <leo@guchat.com.au>
2025-08-26 12:36:14 +1000

    Try full-scrren method

---
+++ src/types/fullscreen.ts
@@ -0,0 +1,19 @@
+// TypeScript declarations for fullscreen API
+declare global {
+  interface Document {
+    webkitFullscreenElement?: Element;
+    mozFullScreenElement?: Element;
+    msFullscreenElement?: Element;
+    webkitExitFullscreen?: () => Promise<void>;
+    mozCancelFullScreen?: () => Promise<void>;
+    msExitFullscreen?: () => Promise<void>;
+  }
+
+  interface HTMLElement {
+    webkitRequestFullscreen?: () => Promise<void>;
+    mozRequestFullScreen?: () => Promise<void>;
+    msRequestFullscreen?: () => Promise<void>;
+  }
+}
+
+export {};
